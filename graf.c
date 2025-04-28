41c76e05a2bc196b68d34726336d9e4bc87b500c
mikolaj45 <01197795@pw.edu.pl>
2025-04-29 01:19:03 +0200

    wiecej poprawek2

--- graf.c
+++ graf.c
@@ -262,30 +262,45 @@ void save_graph_to_csrrg(Graph *graph, const char *filename) {
     // 1. max_vertices
     fprintf(fp, "%d\n", graph->max_vertices);
 
-    // 2. col_index (oryginalne z pliku)
-    for (int i = 0; i < graph->num_vertices + graph->row_ptr[graph->num_vertices]; i++) {
-        fprintf(fp, "%d;", graph->col_index[i]);
+    /// 2. col_index
+    int col_len = graph->row_ptr[graph->num_vertices];
+    for (int i = 0; i < col_len; i++) {
+        if (i != col_len - 1)
+            fprintf(fp, "%d;", graph->col_index[i]);
+        else
+            fprintf(fp, "%d", graph->col_index[i]); // ostatni bez średnika
     }
     fprintf(fp, "\n");
 
-    // 3. row_ptr (oryginalne z pliku)
+    // 3. row_ptr
     for (int i = 0; i <= graph->num_vertices; i++) {
-        fprintf(fp, "%d;", graph->row_ptr[i]);
+        if (i != graph->num_vertices)
+            fprintf(fp, "%d;", graph->row_ptr[i]);
+        else
+            fprintf(fp, "%d", graph->row_ptr[i]); // ostatni bez średnika
     }
     fprintf(fp, "\n");
 
-    // 4. neighbors (nowo wygenerowane)
-    int neighbors_count = row_ptr_out[graph->num_vertices]; // ile elementów
-    for (int i = 0; i < neighbors_count; i++) {
-        fprintf(fp, "%d;", neighbors_out[i]);
+
+    // 4. component
+    for (int i = 0; i < graph->num_vertices; i++) {
+        if (i != graph->num_vertices - 1)
+            fprintf(fp, "%d;", graph->component[i]);
+        else
+            fprintf(fp, "%d", graph->component[i]); // ostatni bez średnika
     }
     fprintf(fp, "\n");
 
-    // 5. row_ptr dla neighbors
-    for (int i = 0; i <= graph->num_vertices; i++) {
-        fprintf(fp, "%d;", row_ptr_out[i]);
+
+    // 5. group_assignment
+    for (int i = 0; i < graph->num_vertices; i++) {
+        if (i != graph->num_vertices - 1)
+            fprintf(fp, "%d;", graph->group_assignment[i]);
+        else
+            fprintf(fp, "%d", graph->group_assignment[i]); // ostatni bez średnika
     }
     fprintf(fp, "\n");
+
 
     fclose(fp);
 
